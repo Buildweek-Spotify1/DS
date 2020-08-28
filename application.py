b1a5eb4f0eb4695152c6309c566cb73964e8fe10
henry <henryomarm@gmail.com>
2020-08-27 20:35:49 -0700

    new model

--- application.py
+++ application.py
@@ -1,73 +1,130 @@
-
-from flask import Flask, request, render_template, jsonify
-from sklearn.neighbors import NearestNeighbors
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import base64
-import io
+import spotipy
+from joblib import load
+from flask import Flask, request, jsonify
+from spotipy.oauth2 import SpotifyClientCredentials
 
+def create_app():
+    '''
+    Create the app, load the model and lookup table, and initiate the spotify client
+    '''
+    app = Flask(__name__)
+    client_credentials_manager = SpotifyClientCredentials(client_id='8da7b70800a34cecbb89a72541dead74', client_secret='471d09ad2d0b4de5831d99f5063c09c8')
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    nn = load('/home/henry/Documents/repos/DS/spotify2.joblib')
+    id_map = np.array(pd.read_csv('/home/henry/Documents/repos/DS/MusicWithGenresFil.csv')).reshape(1,-1)[0]
+    data = pd.read_csv('/home/henry/Documents/repos/DS/train.csv').values
+    features = ['danceability',
+                'energy',
+                'key',
+                'loudness',
+                'mode',
+                'speechiness',
+                'acousticness',
+                'instrumentalness',
+                'liveness',
+                'valence',
+                'tempo']
+    @app.route('/', methods=['GET'])
+    def index():
+        return 'Hi'
 
+    @app.route('/song', methods=['GET'])
+    def song_info():
+        '''
+        This route formats user input, uses it to find the song in the 
+        spotify api, then returns information about it as a json object
+        '''
+        title = request.args.get('title')
+        artist = request.args.get('artist')
 
-df = pd.read_csv("https://raw.githubusercontent.com/Buildweek-Spotify1/DS/master/MusicWithGenresFiltered.csv")
-def process_input(song_id, return_json = True):
-    c = ["duration_ms", "index", "genre", "artist_name", "track_id", "track_name", "key", "mode"] # Columns to Omit
-    song = df[df["track_id"] == song_id].iloc[0] # Get Song
-    df_selected = df.copy()
-    if not pd.isnull(song["genre"]): # If genre, set subset to only genre
-        df_selected = df[df["genre"] == song["genre"]]
-    nn = NearestNeighbors(n_neighbors=31, algorithm="kd_tree") # Nearest Neighbor Model
-    nn.fit(df_selected.drop(columns=c))
-    song = song.drop(index=c)
-    song = np.array(song).reshape(1, -1)
-    if return_json is False:
-        return df_selected.iloc[nn.kneighbors(song)[1][0][1:]] # Return results as df
-    return df_selected.iloc[nn.kneighbors(song)[1][0][1:]].to_json(orient="records") # Return results as json
+        song = sp.search(f'{title} {artist}', type='track', limit=1)
+        info = {'id': song['tracks']['items'][0]['id'],
+                'title': song['tracks']['items'][0]['name'],
+                'artist': song['tracks']['items'][0]['artists'][0]['name'],
+                'album': song['tracks']['items'][0]['album']['name']
+                }
+        return jsonify(info)
 
-app = Flask(__name__)
+    @app.route('/suggestions', methods=['GET'])
+    def suggest():
+        '''
+        This endpoint take a user inputted song/artist, retrieves the audio features for 
+        the input song from the spotify api, preprocesses the data input, and retrieves the
+        predictions from our nearest neighbors model.
+        '''
+        # Use the input to retrieve the song id from spotify
+        title = request.args.get('title')
+        artist = request.args.get('artist')
+        song = sp.search(f'{title} {artist}', type='track', limit=1)
+        try:
+            song_id = song['tracks']['items'][0]['id']
+        except:
+            # Return error message if the song is not on spotify
+            return("Sorry, we couldn't find the song you're looking for.")
+        else:
+            # Query the input song's audio features 
+            song_features = sp.audio_features(song_id)[0]
+            x = np.array([song_features[feature] for feature in features]).reshape(1,-1)
+            # Preprocess the input data
+            x[0][2] = x[0][2]/11
+            x[0][3] = x[0][3]/58.882
+            x[0][10] = x[0][10]/249.983
+            # Get the predictions from the model
+            ids = nn.kneighbors(x)[1][0]
+            # Map predictions to their spotify id
+            ids = [id_map[id] for id in ids]
+            # Retrieve the song information and audio features, return them as a json object
+            s = sp.tracks(ids)
+            s = [s['tracks'][ind] for ind in range(len(s['tracks']))]
+            s = [{'id': t['id'],
+                  'title': t['name'],
+                  'artist': t['artists'][0]['name'],
+                  'album': t['album']['name'],
+                  'image': t['album']['images'][1]['url']}for t in s]
+            af = sp.audio_features(ids)
+            af = [{feat:af[ind][feat]/(song_features[feat]+.0001) for feat in features} for ind in range(len(af))]
+            suggestions = {'song': song, 'tracks':[{'info':s[ind], 'features':af[ind]} for ind in range(len(af))]}
+            return jsonify(suggestions)
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+    @app.route('/least', methods=['GET'])
+    def least():
+        '''
+        This endpoint takes user input, and processes it in order
+        to return the least similar song from out model
+        '''
+        # Retrieve the song features
+        title = request.args.get('title')
+        artist = request.args.get('artist')
+        song = sp.search(f'{title} {artist}', type='track', limit=1)
+        try:
+            song_id = song['tracks']['items'][0]['id']
+        except:
+            return("Sorry, we couldn't find the song you're looking for.")
+        else:
+            song_features = sp.audio_features(song_id)[0]
+            x = np.array([song_features[feature] for feature in features]).reshape(1,-1)
+            # Preprocess the input
+            x[0][2] = x[0][2]/11
+            x[0][3] = x[0][3]/58.882
+            x[0][10] = x[0][10]/249.983
+            
+            # calculate the distances between input song and training data
+            def distancify(row_of_data):
+                return np.linalg.norm(x - row_of_data)
 
+            distances = np.apply_along_axis(distancify, axis=1, arr=data)
 
-@app.route('/song/<song_id>', methods = ['GET'])
-def song(song_id):
-    """Route for recommendations based on song selected."""
-    return  process_input(song_id) #jsonify(recomendations)
+            index_least_similar_song = np.where(distances == np.amax(distances))[0][0]
 
-@app.route('/favorites',methods = ['GET', 'POST'])
-def favorites():
-    my_dict = request.get_json(force=True)
-    track_list = pd.DataFrame()
-    for i in my_dict.values():
-        track_list = track_list.append(process_input(i,False))
-        track_list.drop_duplicates()
-        return track_list.sample(30).to_json(orient="records")
-
-@app.route('/image/<song_id>', methods = ['GET'])
-def radar_map(song_id):
-    """Route for returning radar graph."""
-    c = ["acousticness", "danceability", "energy", "valence"] # Columns to Show
-    N = len(c)
-    values=df[df["track_id"] == song_id].iloc[0][c].tolist()
-    values += values[:1]
-    print(values)
-    angles = [n / float(N) * 2 * 3.141 for n in range(N)]
-    angles += angles[:1]
-    print(angles)
-    ax = plt.subplot(111, polar=True)
-    plt.xticks(angles[:-1], c, color='grey', size=8)
-    plt.yticks([], [], color="grey", size=7)
-    ax.set_rlabel_position(0)
-    ax.plot(angles, values, linewidth=1, linestyle='solid')
-    ax.fill(angles, values, 'b', alpha=0.1)
-    pic_bytes = io.BytesIO()
-    plt.savefig(pic_bytes, format="png")
-    pic_bytes.seek(0)
-    data = base64.b64encode(pic_bytes.read()).decode("ascii")
-    plt.clf()
-    return "<img src='data:image/png;base64,{}'>".format(data)
-
-if __name__ == "__main__":
-    app.run()
+            id_least = id_map[index_least_similar_song]
+            #return the information of the least similar song
+            s = sp.track(id_least)
+            s = {'id': s['id'],
+                 'title': s['name'],
+                 'artist': s['artists'][0]['name'],
+                 'album': s['album']['name'],
+                 'image': s['album']['images'][1]['url']}
+            return jsonify(s)
+    return app
